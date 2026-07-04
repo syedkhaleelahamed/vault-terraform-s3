988f08792e8be1ee82f9aa85bdfc8b81b84c91d8
Khaleel Ahamed Syed <khaleelahamedsyed@Khaleels-MacBook-Pro.local>
2026-07-04 07:45:55 -0400

    infra: connect s3 remote backend tracking

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "syed-terraform-state-backend" # The bucket you just created manually
+    key            = "global/s3/terraform.tfstate"   # The path inside the bucket where the state file will live
+    region         = "us-east-1"                     # Must match your bucket's region
+    encrypt        = true                            # Encrypts the state file at rest
+  }
+}
+
 resource "aws_s3_bucket" "vault_test_bucket" {
   bucket        = "vault-test-bucket-syed-${random_id.id.hex}"
   force_destroy = true
